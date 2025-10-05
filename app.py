89c1a1b5d08a58d0434c81e75018b583e6115523
vishwesh-01 <patilvishwesh2004@gmail.com>
2025-10-05 17:13:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -202,5 +202,7 @@ def handle_update_room_info(data):
 def handle_disconnect():
     handle_leave_room()
 
+# if __name__ == '__main__':
+#     socketio.run(app, debug=True, host='0.0.0.0', port = 5000)
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port = 5000)
+     socketio.run(app)
